f1c282d5ce8ee1e4ac1b0f43f20953233c67f561
fruppel <ruppel.florian@gmail.com>
2025-02-14 09:38:58 +0100

    Add basic form

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Post;
+use App\Form\Type\PostType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +11,25 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class PostController extends AbstractController
 {
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager
+    ) {}
+
     #[Route('/post/index', name: 'post_index')]
-    public function index(EntityManagerInterface $entityManager): Response
+    public function index(): Response
     {
         return $this->render(
             'post/index.html.twig',
-            ['posts' => $entityManager->getRepository(Post::class)->findAll()]
+            ['posts' => $this->entityManager->getRepository(Post::class)->findAll()]
+        );
+    }
+
+    #[Route('/post/create', name: 'post_create')]
+    public function create(): Response
+    {
+        return $this->render(
+            'post/create.html.twig',
+            ['form' => $this->createForm(PostType::class, new Post())]
         );
     }
 }
---
+++ src/Form/Type/PostType.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class PostType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)
+            ->add('body', TextType::class)
+            ->add('save', SubmitType::class);
+    }
+}
